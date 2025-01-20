c49e7f101d69f1510108fedd21599a04adff248f
davidlara089 <148598977+davidlara089@users.noreply.github.com>
2025-01-20 17:53:33 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navbarLinks = document.querySelectorAll('.navbar a');
+
+    navbarLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
